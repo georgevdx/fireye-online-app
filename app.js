0d71f7289c91fd14b2520a7360a4d751f598618e
georgevdx <georgevdx@gmail.com>
2026-04-20 20:38:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -429,32 +429,12 @@ function generateReport() {
     } else {
      notAnsweredCount++;
     }
-    let answerClass = '';
-
-if (answer === 'No') {
-  answerClass = 'answer-no';
-} else if (answer === 'Yes') {
-  answerClass = 'answer-yes';
-} else if (answer === 'N/A') {
-  answerClass = 'answer-na';
-}
-
-let answerClass = '';
-
-if (answer === 'No') {
-  answerClass = 'answer-no';
-} else if (answer === 'Yes') {
-  answerClass = 'answer-yes';
-} else if (answer === 'N/A') {
-  answerClass = 'answer-na';
-}
-
-answersHtml += `
-  <div class="report-answer ${answerClass}">
-    <strong>${item["Item Number"]}. ${item["Checklist Item"]}</strong><br>
-    Answer: ${escapeHtml(answer)}
-  </div>
-`;
+    answersHtml += `
+      <div class="report-answer">
+        <strong>${item["Item Number"]}. ${item["Checklist Item"]}</strong><br>
+        Answer: ${escapeHtml(answer)}
+      </div>
+    `;
   });
 
   const totalItems = selectedChecklist.length;
