718c36f3895ac9ddd67e05c91cd719cd9ede6493
georgevdx <georgevdx@gmail.com>
2026-05-04 13:35:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -165,17 +165,28 @@ if (!projectNameField || !projectAddressField|| !gpsField|| !inMallField || !mal
   function exportReport() {
   generateReport(); // maak seker report is nuut
 
-  const element = document.getElementById('reportSection');
+  const element = document.getElementById('reportContent');
 
   const projectName = getEl('projectName').value.trim() || 'Inspection';
 
   const opt = {
-    margin:       10,
-    filename:     `Fireye_Report_${projectName}.pdf`,
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
-    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-  };
+  margin: 8,
+  filename: `Fireye_Report_${projectName}.pdf`,
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: {
+    scale: 2,
+    useCORS: true,
+    scrollY: 0
+  },
+  jsPDF: {
+    unit: 'mm',
+    format: 'a4',
+    orientation: 'portrait'
+  },
+  pagebreak: {
+    mode: ['avoid-all', 'css', 'legacy']
+  }
+};
 
   html2pdf().set(opt).from(element).save();
 }
