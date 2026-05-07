3456f2a1a52d0cfd184212b7f5c457ee68333d59
georgevdx <georgevdx@gmail.com>
2026-05-07 15:10:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let inspectionTemplates = {};
 let currentProjectId = null;
 let currentPhotos = [];
 
-const SUPABASE_URL = "ispsdmglyylcwkufphnv";
+const SUPABASE_URL = "https://ispsdmglyylcwkufphnv.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzcHNkbWdseXlsY3drdWZwaG52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNzkwNDUsImV4cCI6MjA5MTc1NTA0NX0.Uy_DcmodOBvZf_WMOtnZwAh4ZQeJIbS9ojBw8DzNXhk";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
